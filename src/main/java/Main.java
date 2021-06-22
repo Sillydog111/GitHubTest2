c9433f249ca29c363cac00327c35b342c3396c12
Wuchenxin_pro <194966_glo@qq.com>
2021-06-22 11:42:02 +0800

    hot_fix chongtu commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git2");
-        System.out.println("hello git3");
+        System.out.println("hello git3-3");
         System.out.println("hello git4");
     }
 }
